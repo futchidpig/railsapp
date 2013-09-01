eecd21fd33418fea8c4122b91acf911d549c28f9
Dylan <futchidpig@hotmail.com>
2013-09-01 13:12:52 +1000

    slightly dynamic pages added

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,6 +8,37 @@ describe "Static pages" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
     end
-  end
+
+    it "should have the title 'Help'" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Home")
+    end
+ end
+
+describe "Help page" do
+	it "should have the content'Help'" do
+	 visit '/static_pages/help'
+	 expect(page).to have_content('Help')
+         end
+
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_title("Help")
+    end
+ end
+
+
+  describe "About page" do
+
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About')
+    end
+
+it "should have the title 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_title("About")
+    end
+ end
 end
 
